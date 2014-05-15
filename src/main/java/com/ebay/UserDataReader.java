da5c87d48340bc8f1f4d28dd596093aac44d2903
lenkina9 <Navahonskayaee@gmail.com>
2014-05-15 23:04:47 +0300

    Logging

--- src/main/java/com/ebay/UserDataReader.java
+++ src/main/java/com/ebay/UserDataReader.java
@@ -1,6 +1,7 @@
 package com.ebay;
 
 import com.ebay.entity.User;
+import org.apache.log4j.Logger;
 
 import java.io.*;
 import java.text.ParseException;
@@ -10,6 +11,8 @@ import java.util.TreeSet;
 
 public class UserDataReader {
     private final String fileName;
+    private static Logger logger = Logger.getLogger(UserDataReader.class);
+    private String userInfoLine;
 
     public UserDataReader(String fileName) {
         this.fileName = fileName;
@@ -17,23 +20,20 @@ public class UserDataReader {
 
     public Set readUsers() {
         TreeSet<User> result = new TreeSet<User>();
-        Scanner scanner = null;
-        try {
-            InputStream inputStream = UserDataReader.class.getResourceAsStream(fileName);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-            scanner = new Scanner(reader);
-            while (scanner.hasNextLine()) {
-                User user = DataParser.parse(scanner.nextLine());
+
+        InputStream inputStream = UserDataReader.class.getResourceAsStream(fileName);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        Scanner scanner = new Scanner(reader);
+        while (scanner.hasNextLine()) {
+            try {
+                userInfoLine = scanner.nextLine();
+                User user = DataParser.parse(userInfoLine);
                 result.add(user);
-            }
-        } catch (ParseException e) {
-            return result;
-        }
-        finally {
-            if (scanner != null) {
-                scanner.close();
+            } catch (ParseException e) {
+                logger.error("User info " + userInfoLine + " cannot be parsed: " + userInfoLine);
             }
         }
+        scanner.close();
         return result;
     }
 }
