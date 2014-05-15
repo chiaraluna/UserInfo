3bd8d407024257c959606264b2b3016ac369c015
lenkina9 <Navahonskayaee@gmail.com>
2014-05-16 02:07:28 +0300

    Implemented: Who is the oldest person in the address book?

--- src/main/java/com/ebay/UserService.java
+++ src/main/java/com/ebay/UserService.java
@@ -3,11 +3,13 @@ package com.ebay;
 import com.ebay.entity.Gender;
 import com.ebay.entity.User;
 
+import java.util.GregorianCalendar;
 import java.util.List;
 
 public class UserService {
 
     private UserInfoHolder userInfoHolder;
+    private User theOldest = null;
 
     public UserService() {
         userInfoHolder = new UserInfoHolder();
@@ -25,5 +27,25 @@ public class UserService {
 
     public List<User> getUsers() {
         return userInfoHolder.getUsers();
+    }
+
+    public User findTheOldestMan() {
+        if (theOldest != null) {
+            return theOldest;
+        }
+
+        List<User> users = getUsers();
+
+        if (users == null || users.isEmpty()) {
+            return null;
+        }
+
+        theOldest = users.get(0);
+        for (User user: getUsers()) {
+            if (user.dob.before(theOldest.dob)) {
+                theOldest = user;
+            }
+        }
+        return theOldest;
     }
 }
--- src/test/java/com/ebay/UserServiceTest.java
+++ src/test/java/com/ebay/UserServiceTest.java
@@ -1,16 +1,30 @@
 package com.ebay;
 
 import com.ebay.entity.Gender;
+import com.ebay.entity.User;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class UserServiceTest {
+
+    private static UserService userService;
+
+    @BeforeClass
+    public static void init() {
+        userService = new UserService();
+    }
+
     @Test
     public void testFindMaleCount() {
-        UserService userService = new UserService();
         int actualMalesCount = userService.countByGender(Gender.Male);
         assertEquals(3, actualMalesCount);
     }
 
+    @Test
+    public void testFindTheOldestMan() {
+        User actualUser = userService.findTheOldestMan();
+        assertEquals("Wes Jackson", actualUser.name);
+    }
 }
